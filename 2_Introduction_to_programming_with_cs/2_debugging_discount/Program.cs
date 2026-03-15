70c5ebc73326a522b7ca72cd74a5024c5c705ed0
Kenayv <grosicki.jan@gmail.com>
2026-03-15 13:59:37 +0100

    completed module 2 debugging excercises

---
+++ 2_Introduction_to_programming_with_cs/2_debugging_discount/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    // Method to calculate the final price after a discount
+    public static double ApplyDiscount(double price, double discountPercentage)
+    {
+        return price - ( price * discountPercentage / 100 );
+    }
+
+    public static void Main()
+    {
+        double finalPrice = ApplyDiscount(1000, 15);
+        Console.WriteLine("The final price is: " + finalPrice);
+    }
+}
