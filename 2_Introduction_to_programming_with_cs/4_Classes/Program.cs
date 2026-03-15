6b047bedf1115025e110248a9660c6f0be2c48aa
Kenayv <grosicki.jan@gmail.com>
2026-03-15 14:13:42 +0100

    simple classes

---
+++ 2_Introduction_to_programming_with_cs/4_Classes/Program.cs
@@ -0,0 +1,52 @@
+﻿
+class Calculator
+{
+    public double Add(double a, double b)
+    {
+        return a + b;
+    }
+}
+
+class NumberDisplay
+{
+    public void GisplayNumbers(int[] numbers)
+    {
+        if(numbers.Length < 10) return;
+
+        for(int i = 0; i<10; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
+
+class UserInput()
+{
+    public void GreetUser()
+    {
+        Console.WriteLine("username: ");
+        string username = Console.ReadLine();
+        Console.WriteLine("Greetings " + username);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       double num1 = 2;
+       double num2 = 4;
+
+       Calculator calc = new Calculator();
+
+       Console.WriteLine(calc.Add(num1, num2));
+
+       NumberDisplay nd = new NumberDisplay();
+
+       nd.GisplayNumbers([1,2,3,4,5,5,6,7,8,9,10]);
+
+       UserInput ui = new UserInput();
+
+       ui.GreetUser();
+    }
+}
