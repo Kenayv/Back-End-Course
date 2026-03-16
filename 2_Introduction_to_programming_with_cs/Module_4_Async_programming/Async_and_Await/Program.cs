4803fc345b93a38a334e8fc96a352ef661b5c73e
Kenayv <grosicki.jan@gmail.com>
2026-03-16 14:52:19 +0100

    Async and await - Task handling

---
+++ 2_Introduction_to_programming_with_cs/Module_4_Async_programming/Async_and_Await/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+
+    public static async Task DownloadDataAsync()
+    {
+        try
+        {
+            await Task.Delay(750);
+
+            int a = 0;
+            int b = a/a; // Simulates async task throwing an exception;
+
+            await Task.Delay(750);
+
+            Console.WriteLine("Data downloaded"); 
+        } 
+        catch (Exception)
+        {
+            Console.WriteLine("error downloading data1");
+        }
+        
+
+    }
+    public static async Task DownloadDataAsync2()
+    {
+        await Task.Delay(500);
+        Console.WriteLine("Data downloaded2");
+
+    }
+    public static async Task DownloadDataAsync3()
+    {
+        await Task.Delay(2500);
+        Console.WriteLine("Data downloaded3");
+
+    }
+
+    static async Task Main(string[] args)
+    {
+        List<Task> tasks =
+        [
+            DownloadDataAsync(),
+            DownloadDataAsync2(), 
+            DownloadDataAsync3() 
+        ];
+        
+        await Task.WhenAll(tasks);
+    }
+}
