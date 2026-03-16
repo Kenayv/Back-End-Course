d77e56bf6838a08db70089664ccf3057febe1753
Kenayv <grosicki.jan@gmail.com>
2026-03-16 12:41:00 +0100

    inheritance and polymorphism

---
+++ 2_Introduction_to_programming_with_cs/Module_3_OOP_Csharp/Inheritance_And_Polymorphism/Program.cs
@@ -0,0 +1,80 @@
+﻿
+public interface IAnimal
+{
+    void Eat();
+}
+
+class Animal : IAnimal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("hee hee");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Shee Shee");        
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("woof");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("what the dog doing");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("meow");
+    }
+
+     public void Eat()
+    {
+        Console.WriteLine("eat");
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.MakeSound();
+        cat.MakeSound();
+
+        dog.Eat();
+        cat.Eat();
+
+
+        Console.Write("\n\n");
+
+        List<Animal> list =
+        [
+            new Dog(),
+            new Cat(),
+            new Dog(),
+            new Cat(),
+            new Dog(),
+            new Cat(),
+            new Dog(),
+            new Cat(),
+        ];
+
+        foreach(var a in list)
+        {
+            a.MakeSound();
+        }
+    }
+}
