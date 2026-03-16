74739b6cf341975a2360fff8f7b1043cc17de0fd
Kenayv <grosicki.jan@gmail.com>
2026-03-16 14:12:13 +0100

    module 3 completed

---
+++ 2_Introduction_to_programming_with_cs/Module_3_OOP_Csharp/Implementing_Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Runtime.CompilerServices;
+
+class Person
+{
+    public string name;
+    public int age;
+
+    public void greet()
+    {
+        Console.WriteLine("hejka " + name);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+            Person person = new Person();
+
+            person.age = 19;
+            person.name = "name";
+
+            person.greet();
+    }
+}
