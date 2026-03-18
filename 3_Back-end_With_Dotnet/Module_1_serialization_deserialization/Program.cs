720ff0ede20a4af6c8564d8f9d837fe53f5c5745
Kenayv <grosicki.jan@gmail.com>
2026-03-18 14:52:08 +0100

    serialization, deserialization + fixing sln and directory errors

---
+++ 3_Back-end_With_Dotnet/Module_1_serialization_deserialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml;
+using Newtonsoft.Json;
+public class Person
+{
+    public string Name {get;set;}
+    public int Age {get;set;}
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string json = "{\"Name\": \"Juan Pablo\", \"Age\": 20}";
+
+        Person person = JsonConvert.DeserializeObject<Person>(json);
+        Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+
+        Person newPerson = new Person() {Name = "serialization", Age=13};
+
+        string newPersonJson = JsonConvert.SerializeObject(newPerson);
+        
+        Console.WriteLine(newPersonJson);
+    }
+}
